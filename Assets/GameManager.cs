76f7e76e2c00c04c73b795cbc0b6cc1cb9b599b0
Evann <evann.jacquot@gmail.com>
2024-01-10 11:43:55 +0100

    [feat] Enemy wave spawner
    - Spawner
    - The number of active spawner changes depending on the wave number.
    - we can chose how many enemies we want to spawn

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,12 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private GameObject[] enemySpawnerGo;
-    private int nbreOfSpawner;
+    [SerializeField] private GameObject enemy;
 
-    private int waveCount;
+    private GameObject[] enemySpawnerGo;
+
+    [SerializeField] private int waveCount;
+    [SerializeField] private int howManyEnemies = 2;
     private int enemiesRemaining = 0;
+
+    private bool isCallingNewWave = false;
 
     public static GameManager instance;
 
@@ -14,20 +19,75 @@ public class GameManager : MonoBehaviour
     {
         instance = this;
         enemySpawnerGo = GameObject.FindGameObjectsWithTag("EnemySpawner");
-        nbreOfSpawner = enemySpawnerGo.Length;
+    }
+
+    private void Update()
+    {
+        if (enemiesRemaining == 0 && !isCallingNewWave)
+        {
+            isCallingNewWave = true;
+            NewWave();
+        }
     }
 
     void NewWave()
     {
+        List<GameObject> spawnerList = new List<GameObject>();
+        spawnerList.Clear();
 
+        if (waveCount == 0)
+        {
+            spawnerList.Add(enemySpawnerGo[0]);
+        }
+        else if (waveCount > 0 && waveCount <= 5)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                spawnerList.Add(enemySpawnerGo[i]);
+            }
+        }
+        else if (waveCount > 5 && waveCount <= 8)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                spawnerList.Add(enemySpawnerGo[i]);
+            }
+        }
+        else if (waveCount > 8 && waveCount <= 10)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                spawnerList.Add(enemySpawnerGo[i]);
+            }
+        }
+        else if (waveCount > 10 && waveCount <= 12)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                spawnerList.Add(enemySpawnerGo[i]);
+            }
+        }
+        else if (waveCount > 12)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                spawnerList.Add(enemySpawnerGo[i]);
+            }
+        }
+
+        SpawnEnemy(howManyEnemies, spawnerList);
+
+        waveCount++;
+        howManyEnemies = Mathf.RoundToInt(howManyEnemies * 1.5f);
     }
 
-    void SpawnEnemy(int howManyEnemies, GameObject[] Spawner)
+    void SpawnEnemy(int howManyEnemies, List<GameObject> spawnerList)
     {
+        enemiesRemaining = howManyEnemies;
         for (int i = 0; i < howManyEnemies; i++)
         {
-            int spawner = Random.Range(0, Spawner.Length);
-            //faire spawner un ennemi
-        }
+            int spawner = Random.Range(0, spawnerList.Count);
+            Instantiate(enemy, spawnerList[spawner].transform.position, Quaternion.Euler(0, 0, 0));
+        }   
     }
 }
