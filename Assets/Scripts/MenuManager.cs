a9bc9f0b7e15870305ad7ee00d4317d049b1616c
Evann <evann.jacquot@gmail.com>
2024-01-12 10:06:13 +0100

    [add] Main menu
    menu manager
    audio manager

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    static MenuManager instance;
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
