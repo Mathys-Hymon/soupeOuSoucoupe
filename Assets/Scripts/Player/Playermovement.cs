52665d8cb983c2f334520020a046f17616aa33b1
Mathys Hymon <mathys.hymon@gmail.com>
2024-01-12 11:20:08 +0100

    [fix] movement

--- Assets/Scripts/Player/Playermovement.cs
+++ Assets/Scripts/Player/Playermovement.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class Playermovement : MonoBehaviour
 {
     public static Playermovement instance;
-    [SerializeField] private float walkspeed, runSpeed, jumpForce;
+    [SerializeField] private float walkspeed, runSpeed, jumpForce, airControl;
     [SerializeField] LayerMask ground;
     [SerializeField] Transform groundCheck;
 
@@ -26,11 +27,11 @@ public class Playermovement : MonoBehaviour
         input = context.ReadValue<Vector2>();
     }
 
-    public void jumpInput()
+    public void jumpInput(InputAction.CallbackContext context)
     {
-        if (isGrounded)
+        if (isGrounded && context.ReadValueAsButton())
         {
-            rb.velocity = new Vector3(0, jumpForce, 0);
+            rb.AddForce(Vector3.up * jumpForce * Time.deltaTime * 1500, ForceMode.Force);
         }
     }
 
@@ -51,12 +52,40 @@ public class Playermovement : MonoBehaviour
 
     private void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, ground);
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, ground);
     }
 
     private void FixedUpdate()
     {
-        Vector3 movement = new Vector3((transform.forward.x * input.y * speed * Time.deltaTime) + (transform.right.x * input.x * speed * Time.deltaTime), 0, (transform.forward.z * input.y * speed * Time.deltaTime) + (transform.right.z * input.x * speed * Time.deltaTime));
-        rb.MovePosition(transform.position + movement);
+        float friction = airControl;
+        if (isGrounded)
+        {
+            friction = 1;
+            rb.drag = 8;
+        }
+        else
+        {
+            if(speed == runSpeed)
+            {
+                friction = airControl * 0.2f;
+            }
+            else
+            {
+                friction = airControl * 0.4f;
+            }
+            
+            rb.drag = 0.05f;
+        }
+
+        if(rb.velocity.y < 0 && !isGrounded)
+        {
+            rb.mass += Time.deltaTime*5;
+        }
+        else
+        {
+            rb.mass = 1;
+        }
+        Vector3 movement = new Vector3((transform.forward.x * input.y) + (transform.right.x * input.x), 0, (transform.forward.z * input.y) + (transform.right.z * input.x));
+        rb.AddForce(movement.normalized * speed * friction * Time.deltaTime * 500, ForceMode.Force);
     }
 }
