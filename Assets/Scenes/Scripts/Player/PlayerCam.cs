c653fa385e4371c69b15174c8d5c4d9583bd4a62
Anthony.B <anthony.boll@artfx.fr>
2024-01-17 20:14:54 +0100

    Revert "Merge branch 'Rémy_branch' into Branthony"
    This reverts commit c83b16c5ea4bb26fe5bf29063169cc882e0c3ba3, reversing
    changes made to 3fcaf0fdfe2366f543fc0e82019e4d39290d0697.

--- Assets/Scenes/Scripts/Player/PlayerCam.cs
+++ Assets/Scenes/Scripts/Player/PlayerCam.cs
@@ -1,55 +1,26 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerCam : MonoBehaviour
 {
-    public static PlayerCam instance;
-
-    [SerializeField] private float mouseSensibility = 1f;
+    [SerializeField] private float mouseSensibility = 2f;
     private float cameraVerticalRotation;
-    private float offsetY;
-    private Vector2 input;
+
     private void Start()
     {
-        instance = this;
-
-        offsetY = transform.position.y - Playermovement.instance.transform.position.y;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
-
-    public void mousePosition(InputAction.CallbackContext context)
-    {
-        input = context.ReadValue<Vector2>();
-    }
-    
-    public Vector2 getMouseInput()
-    {
-        return input;
-    }
-    public RaycastHit AimCenter()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, ~Playermovement.instance.gameObject.layer))
-        {
-            return hit;
-        }
-        else
-        {
-            return hit;
-        }
-        
-    }
-
     private void Update()
     {
-        transform.position = Playermovement.instance.transform.position + new Vector3( transform.forward.x*0.2f, offsetY, transform.forward.z*0.2f);
+        transform.position = Playermovement.instance.transform.position;
 
-        cameraVerticalRotation -= input.y *10 *  mouseSensibility * Time.deltaTime;
-        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 75f);
+        float inputX = Input.GetAxis("Mouse X")*mouseSensibility;
+        float inputY = Input.GetAxis("Mouse Y")*mouseSensibility;
+
+        cameraVerticalRotation -= inputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
         transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
 
-        Playermovement.instance.transform.Rotate(Vector3.up * 10 * input.x * mouseSensibility * Time.deltaTime);
-
+        Playermovement.instance.transform.Rotate(Vector3.up * inputX);
     }
 }
