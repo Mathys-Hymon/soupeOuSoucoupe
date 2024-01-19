49047204b927cd0842043b8457e357bee7ebd592
TortueFou <mathys.hymon@gmail.com>
2024-01-19 16:32:15 +0100

    [feat] added item spawner

---
+++ Assets/Scripts/SpawnerScript/ItemSpawnerText.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using TMPro;
+
+public class ItemSpawnerText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI distanceText;
+    void Update()
+    {
+        transform.LookAt(Playermovement.instance.transform.position);
+        float distance = Vector3.Distance(transform.position, Playermovement.instance.transform.position);
+        distanceText.text = Mathf.RoundToInt(distance) + "m";
+        transform.localScale = new Vector3(0.001f * distance / 10f, 0.001f * distance / 10f, 0.001f * distance / 10f);
+    }
+}
